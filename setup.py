06b7bd931daa566059ff1259e49d69850cd154ac
Jarrod Chesney <jarrod.chesney@synerty.com>
2016-11-28 11:07:33 +1000

    Setup for pypi package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'rapui',
+  packages = ['rapui'],
+  version = '0.1',
+  description = 'Synerty utility classes for serving a static site with twisted.web with user permissions.',
+  author = 'Synerty',
+  author_email = 'contact@synerty.com',
+  url = 'https://github.com/Synerty/rapui',
+  download_url = 'https://github.com/Synerty/rapui/tarball/0.1',
+  keywords = ['twisted', 'resource', 'file', 'download', 'synerty'],
+  classifiers = [],
+)
