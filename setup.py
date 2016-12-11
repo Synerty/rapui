a6bde1e5fc4a1c0380469b5105dee3f0a1837b21
Jarrod Chesney <jarrod.chesney@synerty.com>
2016-12-11 13:25:12 +1000

    Added variables for version in setup.py Added pipbuild

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from distutils.core import setup
 
+package_name = "txhttputil"
+package_version = '0.1.4'
+
 setup(
     name='txhttputil',
     packages=['txhttputil', 'txhttputil.downloader', 'txhttputil.login_page', 'txhttputil.site', 'txhttputil.util'],
     package_data={'txhttputil.login_page': ['*.xml']},
-    version='0.1.5',
+    version=package_version,
     description='Synerty utility classes for serving a static site with twisted.web with user permissions.',
     author='Synerty',
     author_email='contact@synerty.com',
     url='https://github.com/Synerty/txhttputil',
-    download_url='https://github.com/Synerty/txhttputil/tarball/0.1.5',
+    download_url=('https://github.com/Synerty/%s/tarball/%s'
+                  % (package_name, package_version)),
     keywords=['twisted', 'resource', 'file', 'download', 'synerty'],
-    classifiers=[],
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+    ],
 )
