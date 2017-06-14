ee26e4eaeb7e59683d71b76294916c4991eda7bd
Jarrod Chesney <jarrod.chesney@synerty.com>
2017-06-14 21:36:22 +1000

    bugfix for updating package __init__ version

--- publish.sh
+++ publish.sh
@@ -43,7 +43,7 @@ echo "Setting version to $VER"
 sed -i "s;^package_version.*=.*;package_version = '${VER}';"  setup.py
 
 # Update the package version
-sed -i "s;.*version.*;__version__ = '${VER}';" vortex/__init__.py
+sed -i "s;.*version.*;__version__ = '${VER}';" ${PY_PACKAGE}/__init__.py
 
 # Reset the commit, we don't want versions in the commit
 
